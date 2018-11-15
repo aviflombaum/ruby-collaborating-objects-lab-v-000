f9cb19094a98ca62cfaa9a3912222092d02cafe2
Avi Flombaum <avi@flombaum.com>
2018-11-15 00:15:29 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,15 @@
 class Artist
   attr_accessor :name, :songs
-  @@all = [] 
+  @@all = []
+
   def initialize(name)
     @name = name
     @songs = []
   end
 
+  def save
+    @@all << self
+  end
 
   def add_song(song)
     @songs << song
