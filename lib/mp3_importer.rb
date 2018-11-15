4e0ee2fb8691546eda82ebeea98d1727b11b5bec
Avi Flombaum <avi@flombaum.com>
2018-11-15 00:33:31 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,8 +8,8 @@ class MP3Importer
   def files
     long_filenames = Dir[@path+"/*"]
     # ["./spec/fixtures/mp3s/Action Bronson - Larry Csonka - indie.mp3", ""]
-    binding.pry
-    long_filenames.collect{|filename| filename.split("/")}
+
+    long_filenames.collect{|filename| filename.split("/").last}
   end
 
 end
